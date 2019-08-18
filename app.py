376dfc12e4bf051844dfcf10eb9276be6f7b06e8
John Colby <john.b.colby@gmail.com>
2019-08-18 01:28:33 -0700

    Add support for process_gbm

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from celery import Celery
 import os
 import brats_preprocessing.brats_preprocessing as bp
 from celery.utils.log import get_task_logger
+import argparse
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345678'
@@ -13,19 +14,18 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 logger = get_task_logger(__name__)
 
-url        = 'https://air.radiology.ucsf.edu/api/'
-model_path = '/mnt/crypt/jcolby/dcmclass/model.Rdata'
-cred_path  = '/home/jcolby/.air_login.txt'
-endpoint   = 'http://localhost:8082/predictions/unet'
-
-@celery.task
-def sleep_x(secs):
-    os.system(f'sleep {secs}')
+args = argparse.Namespace()
+args.url_air         = 'https://air.radiology.ucsf.edu/api/'
+args.model_path      = '/mnt/crypt/jcolby/dcmclass/model.Rdata'
+args.cred_path       = '/home/jcolby/.air_login.txt'
+args.url_seg         = 'http://localhost:8082/predictions/unet'
+args.mni_mask        = True
+args.do_bias_correct = False
 
 @celery.task
 def cli(acc):
-    mri = bp.tumor_study(acc = acc, model_path = model_path)
-    logger.info(mri.acc)
+    args.acc = acc
+    bp.process_gbm(args)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
