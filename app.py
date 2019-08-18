08cf255f469fd9de6067622be604feb6f5364ee7
John Colby <john.b.colby@gmail.com>
2019-08-18 02:04:16 -0700

    Use rq instead of celery.

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from celery import Celery
 import os
 import brats_preprocessing.brats_preprocessing as bp
-from celery.utils.log import get_task_logger
 import argparse
+from redis import Redis
+import rq
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '12345678'
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-logger = get_task_logger(__name__)
+queue = rq.Queue('brats', connection=Redis.from_url('redis://'))
 
 args = argparse.Namespace()
 args.url_air         = 'https://air.radiology.ucsf.edu/api/'
@@ -22,7 +19,6 @@ args.url_seg         = 'http://localhost:8082/predictions/unet'
 args.mni_mask        = True
 args.do_bias_correct = False
 
-@celery.task
 def cli(acc):
     args.acc = acc
     bp.process_gbm(args)
@@ -33,5 +29,5 @@ def main():
         return render_template('input.html')
     if request.method == 'POST':
         acc = request.form['acc']
-        cli.delay(acc)
+        queue.enqueue('app.cli', acc, job_timeout=2700)
         return redirect(url_for('main'))
