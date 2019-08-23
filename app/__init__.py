eb912fea99249f52bd9422951fae7dbf6ab625ec
John Colby <john.b.colby@gmail.com>
2019-08-23 15:31:46 -0700

    Add bootstrap CSS styling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from redis import Redis
 import rq
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +11,7 @@ app.config.from_object(Config)
 queue = rq.Queue(connection=Redis.from_url(app.config['REDIS_URL']))
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 from app import routes
