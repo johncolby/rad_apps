e51935bc537bb40f5cc9f0d325bc2d675e0c3579
John Colby <john.b.colby@gmail.com>
2019-08-23 15:33:13 -0700

    Keep email field filled upon submit.

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,10 @@ from app.forms import SegmentationForm
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    form = SegmentationForm()
+    email = request.args.get('email') or ''
+    form = SegmentationForm(email=email)
     if form.validate_on_submit():
         queue.enqueue(process_gbm_wrapper, form.data, job_timeout=2700)
-        return redirect(url_for('main'))
         flash(f'Segmentation request submitted for acc # {form.acc.data}')
+        return redirect(url_for('main', email=form.email.data))
     return render_template('input.html', form=form)
