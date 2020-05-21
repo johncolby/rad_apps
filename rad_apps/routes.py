f6e5393ddfc9003822141556a2e3f5db527c17d1
John Colby <john.b.colby@gmail.com>
2020-05-20 19:53:53 -0700

    Allow accession # as query parameter

--- rad_apps/routes.py
+++ rad_apps/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, request, url_for, flash
 from rad_apps import app, app_list, queue
 from .tasks import app_wrapper
 from .forms import ChooseApp, get_form
@@ -9,13 +9,15 @@ def index():
     form = ChooseApp()
     if form.validate_on_submit():
         app_name = form.app_name.data
-        return redirect(url_for('input', app_name=app_name))
+        return redirect(url_for('input', app_name=app_name, acc=request.args.get('acc')))
     return render_template('index.html', form=form)
 
 
 @app.route('/apps/<app_name>', methods=['GET', 'POST'])
 def input(app_name):
     form = get_form(app_name)
+    if form.acc.data is None:
+        form.acc.data = request.args.get('acc')
     long_name = app_list.get_app_info(app_name).long_name
     if form.validate_on_submit():
         queue.enqueue(app_wrapper, app_name, form.data, job_timeout=3600)
